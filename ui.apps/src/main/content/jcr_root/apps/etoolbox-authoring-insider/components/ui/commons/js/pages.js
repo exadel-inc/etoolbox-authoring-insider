9df1a1dc66186e36f464cb83a2be712503a0298c
Stepan Miakchilo <smiakchilo@exadel.com>
2025-06-20 00:08:47 +0300

    [EAI-27] Modified the pages utility with the ability to output markdown

--- ui.apps/src/main/content/jcr_root/apps/etoolbox-authoring-insider/components/ui/commons/js/pages.js
+++ ui.apps/src/main/content/jcr_root/apps/etoolbox-authoring-insider/components/ui/commons/js/pages.js
@@ -14,30 +14,50 @@
 (function (document, ns) {
     'use strict';
 
-    const NON_CONTENT_TAGS = ['script', 'style', 'link', 'iframe', 'object', 'embed'];
-    const NON_EMPTY_TAGS = ['div', 'span', 'section', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
+    const EMBARGOED_ELEMENTS = [
+        'button',
+        'canvas',
+        'embed',
+        'form',
+        'iframe',
+        'input',
+        'link',
+        'meta',
+        'noscript',
+        'object',
+        'script',
+        'style',
+        'template',
+        'svg'
+    ];
 
     /**
      * Contains utility logic for working with HTML content
      */
     ns.pages = {
         /**
-         * Extracts HTML content from the provided AEM page removing non-content tags and empty elements
-         * @param {string} address - Path to the page
+         * Extracts content from the provided AEM page removing non-content tags and empty elements
+         * @param {string} url - Path to the page
+         * @param {object} [options] - Options for extraction
          * @returns {string}
          */
         extractContent
     };
 
-    async function extractContent(url) {
+    async function extractContent(url, options = {}) {
         try {
             const content = await getPageContent(url);
-            return parsePageDom(content);
+            const dom = parseDom(content);
+            return convertDom(dom, options);
         } catch (e) {
             console.error('Failed to extract page content', e);
             return '';
         }
     }
+
+    /* -----------------
+       Content retrieval
+       ----------------- */
 
     async function getPageContent(url) {
         const urlObject = new URL(url);
@@ -51,43 +71,209 @@
         return await ns.http.getText(urlObject.toString());
     }
 
-    function parsePageDom(value) {
-        if (!ns.utils.isString(value)) {
+    function parseDom(text) {
+        if (!ns.utils.isString(text)) {
             return '';
         }
-        try {
-            const dom = new DOMParser().parseFromString(value, 'text/html');
-            return cleanUpDom(dom.querySelector('main') || dom.querySelector('body') || dom.documentElement);
-        } catch (e) {
-            console.warn('Failed to parse content', e);
-            return '';
-        }
+        const dom = new DOMParser().parseFromString(text, 'text/html');
+        return dom.querySelector('main') || dom.querySelector('body') || dom.documentElement;
     }
 
-    function cleanUpDom(value) {
-        if (!value) {
+    function convertDom(dom, options) {
+        if (!dom) {
             return '';
         }
-        NON_CONTENT_TAGS.forEach(tag => {
-            value.querySelectorAll(tag).forEach(element => element.remove());
-        });
-        cleanUpNodes(value);
-        Array.from(value.querySelectorAll(':empty'))
-            .filter(element => NON_EMPTY_TAGS.includes(element.tagName.toLowerCase()))
-            .forEach((element) => element.remove());
-        return value.innerHTML.trim();
+        removeComments(dom);
+        removeElements(dom, options && options.excludeElements);
+        removeInternalAnchors(dom);
+
+        trimTextContent(dom);
+
+        let result;
+        if (options && options.format === 'md') {
+            extractSpanContent(dom);
+            replaceAnchorTags(dom);
+            replaceImgTags(dom);
+            replaceHeadings(dom);
+            replaceParagraphs(dom);
+            replaceLineBreaks(dom);
+            replaceListTags(dom);
+            result = toMarkdown(dom);
+        } else {
+            result = dom.innerHTML;
+        }
+        return result.replace(/\n+/g, options.escapeNewLine ? '\\n' : '\n');
     }
 
-    function cleanUpNodes(value) {
-        const removableAttributes = value.getAttributeNames()
-            .filter((name) => name !== 'role' && !name.startsWith('aria-'));
-        removableAttributes.forEach((name) => value.removeAttribute(name));
-        for (const childNode of value.childNodes) {
-            if (childNode.nodeType === Node.ELEMENT_NODE) {
-                cleanUpNodes(childNode);
-            } else if (childNode.nodeType === Node.TEXT_NODE) {
-                childNode.nodeValue = childNode.nodeValue.trim();
+    /* -------------------------
+       Common conversion methods
+       ------------------------- */
+
+    function removeComments(dom) {
+        const walker = dom.ownerDocument.createTreeWalker(dom, 0x80, null);
+
+        const commentsToRemove = [];
+        let currentNode;
+        while ((currentNode = walker.nextNode())) {
+            commentsToRemove.push(currentNode);
+        }
+
+        commentsToRemove.forEach(commentNode => {
+            if (commentNode.parentNode) {
+                commentNode.parentNode.removeChild(commentNode);
             }
+        });
+    }
+
+    function removeElements(dom, extraElements) {
+        EMBARGOED_ELEMENTS.concat(...(extraElements || [])).forEach(selector => {
+            try {
+                const elements = dom.querySelectorAll(selector);
+                elements.forEach(element => {
+                    if (element.parentNode) {
+                        element.parentNode.removeChild(element);
+                    }
+                });
+            } catch (error) {
+                console.error(`Error removing elements with selector "${selector}":`, error.message);
+            }
+        });
+    }
+
+    function removeInternalAnchors(dom) {
+        const anchors = dom.querySelectorAll('a[href^="#"]');
+        anchors.forEach(anchor => {
+            if (anchor.parentNode) {
+                anchor.parentNode.removeChild(anchor);
+            }
+        });
+    }
+
+    /* -----------------------------
+       Markdown-specific consversion
+       ----------------------------- */
+
+    function extractSpanContent(dom) {
+        let spans;
+        while ((spans = dom.querySelectorAll('span:not(:has(span))')).length > 0) {
+            spans.forEach(span => {
+                if (!span.parentNode) {
+                    return;
+                }
+                if (span.innerHTML !== span.textContent) {
+                    // Contains HTML - replace span with its inner HTML
+                    const fragment = dom.ownerDocument.createRange().createContextualFragment(span.innerHTML);
+                    span.parentNode.insertBefore(fragment, span);
+                } else if (span.textContent.trim().length > 0) {
+                    // Simple text - add spaces before and after
+                    const textContent = ` ${span.textContent.trim()} `;
+                    const textNode = dom.ownerDocument.createTextNode(textContent);
+                    span.parentNode.insertBefore(textNode, span);
+                }
+                span.parentNode.removeChild(span);
+            });
         }
+    }
+
+    function replaceAnchorTags(dom) {
+        const images = dom.querySelectorAll('a[href]');
+        images.forEach(a => {
+            const title = (a.getAttribute('title') || a.textContent).replace(/^\W+|\W+$/g, '');
+            if (title.length > 0) {
+                const textNode = dom.ownerDocument.createTextNode(`[${title}](${a.href})`);
+                a.parentNode && a.parentNode.insertBefore(textNode, a);
+            }
+            a.parentNode && a.parentNode.removeChild(a);
+        });
+    }
+
+    function replaceHeadings(dom) {
+        for (let level = 1; level <= 6; level++) {
+            const headings = dom.querySelectorAll(`h${level}`);
+            headings.forEach(heading => {
+                const content = heading.textContent.trim();
+                const markdownHeading = '\n\n' + '#'.repeat(level) + ' ' + content + '\n';
+
+                const textNode = dom.ownerDocument.createTextNode(markdownHeading);
+                if (heading.parentNode) {
+                    heading.parentNode.insertBefore(textNode, heading);
+                    heading.parentNode.removeChild(heading);
+                }
+            });
+        }
+    }
+
+    function replaceImgTags(dom) {
+        const images = dom.querySelectorAll('img');
+        images.forEach(img => {
+            const altText = img.hasAttribute('alt') ? img.getAttribute('alt').replace(/^\W+|\W+$/g, '') : '';
+            if (altText.length > 0) {
+                const textNode = dom.ownerDocument.createTextNode(`\n![${altText}](${img.src})`);
+                img.parentNode && img.parentNode.insertBefore(textNode, img);
+            }
+            img.parentNode && img.parentNode.removeChild(img);
+        });
+    }
+
+    function replaceLineBreaks(dom) {
+        const lineBreaks = dom.querySelectorAll('br');
+        lineBreaks.forEach(br => {
+            if (br.parentNode) {
+                const textNode = dom.ownerDocument.createTextNode('\n');
+                br.parentNode.insertBefore(textNode, br);
+                br.parentNode.removeChild(br);
+            }
+        });
+    }
+
+    function replaceListTags(dom) {
+        let listElements;
+        while ((listElements = dom.querySelectorAll('ul:not(:has(ul,ol)),ol:not(:has(ul,ol))')).length > 0) {
+            listElements.forEach(list => {
+                const liElements = list.querySelectorAll('li');
+                let markdownList = '';
+                liElements.forEach((li, index) => {
+                    const listItem = li.textContent.trim();
+                    if (listItem) {
+                        let prefix = list.tagName.toLowerCase() === 'ul' ? '- ' : `${index + 1}. `;
+                        if (listItem.startsWith('#')) {
+                            prefix = '';
+                        }
+                        markdownList += `${prefix}${listItem}\n`;
+                    }
+                });
+                if (markdownList) {
+                    const textNode = dom.ownerDocument.createTextNode('\n' + markdownList + '\n');
+                    list.parentNode && list.parentNode.insertBefore(textNode, list);
+                }
+                list.parentNode && list.parentNode.removeChild(list);
+            });
+        }
+    }
+
+    function replaceParagraphs(dom) {
+        const paragraphs = dom.querySelectorAll('p');
+        paragraphs.forEach(p => {
+            const textContent = p.textContent.trim();
+            if (textContent.length > 0) {
+                const textNode = dom.ownerDocument.createTextNode(textContent + '\n');
+                p.parentNode && p.parentNode.insertBefore(textNode, p);
+            }
+            p.parentNode && p.parentNode.removeChild(p);
+        });
+    }
+
+    function trimTextContent(dom) {
+        const walker = dom.ownerDocument.createTreeWalker(dom, 0x4, null);
+        let currentNode;
+        while ((currentNode = walker.nextNode())) {
+            currentNode.textContent = currentNode.textContent.trim().replace(/[\n\r]+/g, ' ');
+        }
+    }
+
+    function toMarkdown(dom) {
+        return dom.textContent
+            .replace(/ +/g, ' ')
+            .replace(/^\s+|\s$/g, '');
     }
 })(document, window.eai = window.eai || {});
